b3199f6a503659e3d307c1f199c9e76dd84dabc7
Vikash Anand <vikashanand338@gmail.com>
2022-07-02 14:49:54 +0530

    Class Component Added & State Property Implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      name : {firstName : 'Vikash', lastName: 'Anand'}
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hi {this.state.name.firstName} {this.state.name.lastName} !
+          </p>
+          <button onClick={() => {
+            this.setState((state, props) => {
+              return {name : {firstName: 'Sradha', lastName: 'Jena'}}
+            }, ()=> {
+              console.log(this.state); // Runs After UpdateState
+            })
+            console.log(this.state); // Synchronous Call, Runs Before UpdateState
+          }}> Change Name </button>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
