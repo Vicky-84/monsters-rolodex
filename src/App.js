53402ac72bd944131169b3e6673908c630c02213
Vikash Anand <vikashanand338@gmail.com>
2022-07-03 12:12:03 +0530

    Monsters Array Mapping Done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react'
 
@@ -8,27 +7,23 @@ class App extends Component {
     super();
 
     this.state = {
-      name : {firstName : 'Vikash', lastName: 'Anand'}
+      monsters: [
+        {name : 'Linda'},
+        {name : 'Frank'},
+        {name : 'Jacky'},
+        {name : 'Devil'}
+      ]
     }
   }
 
   render(){
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name.firstName} {this.state.name.lastName} !
-          </p>
-          <button onClick={() => {
-            this.setState((state, props) => {
-              return {name : {firstName: 'Sradha', lastName: 'Jena'}}
-            }, ()=> {
-              console.log(this.state); // Runs After UpdateState
-            })
-            console.log(this.state); // Synchronous Call, Runs Before UpdateState
-          }}> Change Name </button>
-        </header>
+        {
+          this.state.monsters.map((monster, index)=> {
+            return <h1 key={index}>{monster.name}</h1>
+          })
+        }
       </div>
     )
   }
