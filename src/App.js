99321e7cdd08ce5da09eda57f5ea095f7d4988ca
Vikash Anand <vikashanand338@gmail.com>
2022-07-04 14:28:38 +0530

    Search Field Implemented

--- src/App.js
+++ src/App.js
@@ -7,25 +7,41 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField : ''
     }
   }
 
   componentDidMount(){
     fetch('https://jsonplaceholder.typicode.com/users')
     .then((response)=> response.json())
-    .then((users)=> 
-      this.setState(()=>{ return {monsters: users} }, 
-       ()=>{ console.log(this.state)}
-      )
+    .then((users)=> {
+      this.setState(()=>{ return {monsters: users} })}
     );
   }
 
+  onSearchChange = (event) => {
+      const searchField = event.target.value.toLocaleLowerCase();
+      this.setState(()=>{
+        return {searchField}
+      })
+  }
+
   render(){
+
+    const {monsters, searchField} = this.state;
+    const {onSearchChange} = this;
+
+    const filteredMonsters = monsters.filter((monster)=>{
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    })
+
     return (
       <div className="App">
+        <input className='search-box' type='search' placeholder='Search Monster'
+        onChange={onSearchChange}></input>
         {
-          this.state.monsters.map((monster, index)=> {
+          filteredMonsters.map((monster, index)=> {
             return <h1 key={index}>{monster.name}</h1>
           })
         }
